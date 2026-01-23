2b749836ad89bdb7ee5c9293e424492fa438eded
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-01-23 18:14:35 +0530

    fix: update route for user channel profile while testing; from "/c/:username" to "/@:username".

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -47,7 +47,7 @@ router
    .route("/cover-image")
    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/@:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
