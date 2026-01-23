232722e99dbf4ae64bf85b8104f621d00ee818fe
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-01-23 18:18:11 +0530

    fix: change res.send to res.status for proper response handling in getWatchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -415,7 +415,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
    ]);
 
    return res
-      .send(200)
+      .status(200)
       .json(
          new ApiResponse(
             200,
