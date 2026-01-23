648cb93db2eca4dfbca637415eb09687cfa1f04f
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-01-23 06:29:17 +0530

    fix: fix small error while destructuring refreshToken and changed res.send to res.status.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -178,17 +178,17 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       httpOnly: true,
       secure: true,
    };
-   const { accessToken, newRefreshToken } =
+   const { accessToken, refreshToken } =
       await generateAccessAndRefreshToken(user._id);
 
    return res
-      .send(200)
+      .status(200)
       .cookie("accessToken", accessToken, options)
-      .cookie("refreshToken", newRefreshToken, options)
+      .cookie("refreshToken", refreshToken, options)
       .json(
          new ApiResponse(
             200,
-            { accessToken, refreshToken: newRefreshToken },
+            { accessToken, refreshToken },
             "Access token refreshed successfully"
          )
       );
