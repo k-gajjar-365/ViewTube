255a2054e5bde0503b21e099c1b7f81b4b5b8203
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-01-29 12:21:45 +0530

    feat: implement getUserTweets functionality and improve createTweet error handling

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,32 +1,65 @@
+import mongoose from "mongoose";
 import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { validateMongoId } from "../utils/validateMongoId.js";
 
-const createTweet = asyncHandler(async(req, res) => {
-    
-    const { content } = req.body;
-    const userId = req.user._id;
+const createTweet = asyncHandler(async (req, res) => {
+   const { content } = req.body;
+   const userId = req.user._id;
 
-    if(!content) 
-        throw new ApiError(400, "Tweet content required")
+   if (!content) throw new ApiError(400, "Tweet content required");
 
-    const tweet = await Tweet.create({
-        content,
-        owner: userId
-    });
+   const tweet = await Tweet.create({
+      content,
+      owner: userId,
+   });
 
-    if(!tweet)
-        throw new ApiError(500, "Something went wrong while created tweet");
-    
-    return res
-    .status(201)
-    .json(
-        new ApiResponse(201, tweet, "Tweet created successfully.")
-    )
+   if (!tweet)
+      throw new ApiError(500, "Something went wrong while creating tweet");
 
+   return res
+      .status(201)
+      .json(new ApiResponse(201, tweet, "Tweet created successfully."));
 });
 
-export {
-    createTweet
-}
+const getUserTweets = asyncHandler(async (req, res) => {
+   const { userId } = req.params;
+
+   validateMongoId(userId);
+
+   const user = await User.findById(userId);
+
+   if (!user) throw new ApiError(404, "User not found");
+
+   const tweets = await Tweet.find({
+      owner: new mongoose.Types.ObjectId(userId),
+   });
+
+   const totalTweets = tweets.length;
+
+   if (tweets.length === 0)
+      return res
+         .status(200)
+         .json(
+            new ApiResponse(
+               200,
+               { totalTweets },
+               "No tweets found on this account"
+            )
+         );
+
+   return res
+      .status(200)
+      .json(
+         new ApiResponse(
+            200,
+            { tweets, totalTweets },
+            "Tweets fetched successfully"
+         )
+      );
+});
+
+export { createTweet, getUserTweets };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, getUserTweets } from "../controllers/tweet.controller.js";
 
 const router = Router();
 
@@ -8,5 +8,7 @@ const router = Router();
 router.use(verifyJWT);
 
 router.route("/").post(createTweet);
+
+router.route("/user/:userId").get(getUserTweets);
 
 export default router;
