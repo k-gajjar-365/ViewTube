60126691e8fc7a40d63f4478bb1bde059a454425
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-01-31 14:14:32 +0530

    feat: Add getVideoComments by videoId funcrtionality; add route in comment.route.js

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,6 +5,7 @@ import { isEmptyString } from "../utils/isEmptyString.js";
 import { Video } from "../models/video.model.js";
 import { Comment } from "../models/comment.model.js";
 import { validateMongoId } from "../utils/validateMongoId.js";
+import mongoose from "mongoose";
 
 const addComment = asyncHandler(async (req, res) => {
    const { videoId } = req.params;
@@ -33,7 +34,6 @@ const addComment = asyncHandler(async (req, res) => {
 });
 
 const updateComment = asyncHandler(async (req, res) => {
-
    const { commentId } = req.params;
    const { content } = req.body;
 
@@ -81,4 +81,68 @@ const deleteComment = asyncHandler(async (req, res) => {
       .json(new ApiResponse(200, {}, "Comment deleted successfully"));
 });
 
-export { addComment, deleteComment, updateComment };
+const getVideoComments = asyncHandler(async (req, res) => {
+   const { videoId } = req.params;
+   const { page = 1, limit = 10 } = req.query;
+
+   validateMongoId(videoId);
+
+   const video = await Video.findById(videoId);
+
+   if (!video) throw new ApiError(404, "Video not found");
+
+   const commentsAggregate = Comment.aggregate([
+      {
+         $match: {
+            video: new mongoose.Types.ObjectId(videoId),
+         },
+      },
+      {
+         $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "ownerDetails",
+            pipeline: [
+               {
+                  $project: {
+                     avatar: 1,
+                     username: 1,
+                  },
+               },
+            ],
+         },
+      },
+      {
+         $addFields: {
+            owner: {
+               $first: "$ownerDetails",
+            },
+         },
+      },
+      {
+         $project: {
+            content: 1,
+            owner: 1,
+         },
+      },
+   ]);
+
+   const options = {
+      page: parseInt(page, 10),
+      limit: parseInt(limit, 10),
+   };
+
+   const comments = await Comment.aggregatePaginate(commentsAggregate, options);
+
+   if (comments?.docs?.length === 0)
+      return res
+         .status(200)
+         .json(new ApiResponse(200, {}, "No Comments found on this video"));
+
+   return res
+      .status(200)
+      .json(new ApiResponse(200, comments, "Comments fetched successfully"));
+});
+
+export { addComment, deleteComment, updateComment, getVideoComments };
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -3,13 +3,14 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
    addComment,
    deleteComment,
+   getVideoComments,
    updateComment,
 } from "../controllers/comment.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/:videoId").post(addComment);
+router.route("/:videoId").get(getVideoComments).post(addComment);
 
 router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
 
