73e978d7364a928274dacae7fa46ce74b8df7d71
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-01-31 15:39:03 +0530

    feat: Add getSubscribedChannels functionality that fetches subscribed channels; update route details for clarification.

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { getUserChannelSubscribers } from "../controllers/subscription.controller.js";
+import { getUserChannelSubscribers, getSubscribedChannels } from "../controllers/subscription.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("c/:channelId").get(getUserChannelSubscribers);
+router.route("/channel/:channelId").get(getUserChannelSubscribers);
+
+router.route("/user/:subscriberId").get(getSubscribedChannels);
 
 export default router;
