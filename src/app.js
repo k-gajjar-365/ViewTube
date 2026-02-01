26e58786b76d3a04cb4366f5c796dd8a01f265d8
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-02-01 19:11:49 +0530

    refactor: rename files by replacing "route.js" to "routes.js" to maintian standerization; refactor import name for tweet router in app.js

--- src/app.js
+++ src/app.js
@@ -18,16 +18,16 @@ app.use(cookieParser());
 // routes import
 import userRouter from "./routes/user.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
-import tweetsRouter from "./routes/tweet.routes.js";
-import videoRouter from "./routes/video.route.js";
-import commentRouter from "./routes/comment.route.js";
-import subscriptionRouter from "./routes/subscription.route.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 import likeRouter from "./routes/like.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/playlist", playlistRouter);
-app.use("/api/v1/tweets", tweetsRouter);
+app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
