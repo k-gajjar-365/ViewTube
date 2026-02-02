536762f5bb6d266996f594d6e9da59d0e44d204e
Kirtan Gajjar <w.kgajjar@gmail.com>
2026-02-02 13:39:27 +0530

    fix: removed unnecessary imports in dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,7 +1,5 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { Like } from "../models/like.model.js";
 import { Video } from "../models/video.model.js";
 import mongoose from "mongoose";
 import { Subscription } from "../models/subscription.model.js";
